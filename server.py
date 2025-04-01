ac561942806f8cd169855a1b4b9cda3a7f0881e6
Liana Wadhwani <89544220+lianawadhwani@users.noreply.github.com>
2025-04-01 18:13:54 -0400

    fhdsd

--- server.py
+++ server.py
@@ -132,7 +132,7 @@ def searchRestaurant():
 	# DEBUG: this is debugging code to see what request looks like
 	print(request.args)
 
-@app.route('/view/<int: restaurant_id>')
+@app.route('/view/<int:restaurant_id>')
 def viewRestaurant(restaurant_id): 
     select_restaurant = text("SELECT * FROM Restaurant WHERE restaurant_id = :restaurant_id")
     cursor = g.conn.execute(select_restaurant, {"restaurant_id": restaurant_id})
