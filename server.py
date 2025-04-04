3efd6a4968a921d247c96e3adb7657c6c2b6d980
Liana Wadhwani <89544220+lianawadhwani@users.noreply.github.com>
2025-04-04 16:08:32 -0400

    fdsa

--- server.py
+++ server.py
@@ -150,7 +150,6 @@ def user_info(userName):
 	select_fav_cuisine = text("SELECT * FROM has_fav WHERE userName = :userName")
 	cursor1 = g.conn.execute(select_fav_cuisine, {"userName": userName})
 	cuisines = cursor1.fetchone()  
-	print (cuisines)
 	cursor1.close() 
 	return render_template("user_info.html", userName=userName, cuisines=cuisines)
 
