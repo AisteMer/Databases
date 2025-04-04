9187ccb5e1d467eeb898cfd82f5a5a2eb14c5750
Liana Wadhwani <89544220+lianawadhwani@users.noreply.github.com>
2025-04-04 12:27:02 -0400

    fsaf

--- server.py
+++ server.py
@@ -140,13 +140,12 @@ def searchRestaurant():
 		""")
 	else: 
 		search = text("""
-    	SELECT r.restaurant_id, ac.cuisineName, r.priceTag, r.name, located.zipcode 
+    	SELECT r.restaurant_id, ac.cuisineName, r.priceTag, r.name
     	FROM Restaurant r
     	JOIN ASSIGN_CUISINE ac ON r.restaurant_id = ac.restaurant_id
 		JOIN is_located located ON r.restaurant_id = located.restaurant_id
     	WHERE ac.cuisineName = :user_input
 		OR r.name = :user_input
-		OR located.zipcode = :user_input 
 		""")
 	cursor = g.conn.execute(search, {"user_input": user_input, "user_input_zip": user_input_zip})
 	restaurants=cursor.fetchall(); 
