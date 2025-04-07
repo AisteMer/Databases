96a01239a14e16bbd2dcce7ddc0792bb4132c39f
Liana Wadhwani <89544220+lianawadhwani@users.noreply.github.com>
2025-04-06 21:22:23 -0400

    fsafa

--- server.py
+++ server.py
@@ -367,7 +367,7 @@ def viewRestaurant(restaurant_id):
 	return render_template("displayRestaurant.html", restaurant=restaurant, ratings=ratings, locations=locations, cuisines=cuisines, awards=awards, avg_rating=average_rating, numReviews=numReviews)
 
 
-@app.route('/view/<int:bookmark_id>', methods=['GET','POST'])
+@app.route('/<username>/<int:bookmark_id>', methods=['GET','POST'])
 def addEBookmark(bookmark_id):
 	return render_template("addEBookmark.html", bookmark_id=bookmark_id)
 
