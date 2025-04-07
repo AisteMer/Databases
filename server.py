58f23495c75a55b371947e3d1768cf1b552f2f0a
Liana Wadhwani <89544220+lianawadhwani@users.noreply.github.com>
2025-04-06 21:32:21 -0400

    fdsafsa

--- server.py
+++ server.py
@@ -367,9 +367,9 @@ def viewRestaurant(restaurant_id):
 	return render_template("displayRestaurant.html", restaurant=restaurant, ratings=ratings, locations=locations, cuisines=cuisines, awards=awards, avg_rating=average_rating, numReviews=numReviews)
 
 
-@app.route('/<username>/<int:bookmark_id>', methods=['GET','POST'])
-def addEBookmark(username, bookmark_id):
-	return render_template("addEBookmark.html",username=username, bookmark_id=bookmark_id)
+@app.route('/bookmark', methods=['GET'])
+def addEBookmark():
+	return render_template("addEBookmark.html")
 
 """""
 @app.route('/view/<restaurant>', methods=['GET', 'POST'])
