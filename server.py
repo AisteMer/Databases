6cd522e05e7d590cd5daf641c9b6a62f2d849589
Liana Wadhwani <89544220+lianawadhwani@users.noreply.github.com>
2025-04-06 20:35:38 -0400

    fsafs

--- server.py
+++ server.py
@@ -170,8 +170,8 @@ def user_info(username):
 
 	return render_template("user_info.html", username=username, cuisines=cuisines, friends=friends, username1=username, users=users)
 
-def addUser(username): 
-	return render_template("user_info.html",username=username)
+def addUser(): 
+	return render_template("user_info.html")
 
 	"""
 	request is a special object that Flask provides to access web request information:
